259e89c19e5e45b755a53abc365e598f86f15751
chesahkalu <chesahkalu@gmail.com>
2023-01-11 01:01:55 +0000

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ Python function that add arguments to a Python list
 import sys
 import os
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 
