e7fe6fadeda58aa4e66792d1fd49b99404b37b9d
chesahkalu <chesahkalu@gmail.com>
2023-03-31 03:06:40 +0100

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
-
+""" Finds a peak in a list of unsorted integers
+"""
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: 
+        peak of list_of_integers or None
+    """
+    if len(list_of_integers) == 0:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low+high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low] or list_of_integers
