fb378299b47721a7d751667f27a4c7047020534c
chesahkalu <chesahkalu@gmail.com>
2023-04-01 06:20:31 +0100

    0x11-python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays 
-the value of the variable X-Request-Id in the response header
+"""Sends a request to the URL and displays the value of 
+the variable X-Request-Id in the response header.
 Eg: ./5-hbtn_header.py <URL>
 """
 import sys
