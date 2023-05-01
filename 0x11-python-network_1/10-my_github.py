dffb42f38f3fa397ee245f02b6b1748f137e6b02
chesahkalu <chesahkalu@gmail.com>
2023-05-01 23:03:07 +0100

    requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -13,13 +13,3 @@ if __name__ == "__main__":
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
     
-    """This is a Python script that uses the requests library to make an authenticated GET request to the GitHub API 
-    to retrieve the user information for the authenticated user.
-    
-    The script takes two command-line arguments: the username and password/token for the GitHub account.
-    
-    These arguments are used to create an HTTPBasicAuth object, which is then passed to the auth parameter of 
-    the requests.get() function to authenticate the request.
-    
-    The response from the API is then printed to the console as JSON, and the user ID is extracted from the JSON using 
-    the get() method and printed to the console as well."""
